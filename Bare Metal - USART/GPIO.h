206c568c24dd88e84c3fccbf7649954e07badd6b
faiak_msi_windows <azmainfaiak45@google.com>
2024-03-20 09:11:40 +0600

    added clock setup, gpio setup,timer setup and usart

---
+++ Bare Metal - USART/GPIO.h
@@ -0,0 +1,19 @@
+
+#ifndef __GPIO_H
+#define __GPIO_H
+
+
+#include "stm32f446xx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void GPIOA_config(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ Bare Metal - USART/main.c
@@ -0,0 +1,42 @@
+#include "stm32f446xx.h"
+#include "CLOCK.h"
+#include "GPIO.h"
+#include "Delay.h"
+#include "USART.h"
+
+int main(void){
+	
+	initClock();
+	TIM6Config();
+	GPIOA_config();
+	
+	
+/*	
+	//LED Blinking on the MC Board
+	while(47){
+		GPIOA->BSRR |= (1<<5); //set the PA5 pin
+		
+		delay_ms(100); 
+		
+		GPIOA->BSRR |= ((1<<5)<<16); //reset the PA5 pin
+		
+		delay_ms(100);
+		
+	}
+	
+*/	
+	
+	//
+	
+	UART2_Config();
+	//delay_ms(2000);
+	
+	while(1){
+		
+		UART2_SendChar('C');
+		delay_ms(1000);
+		
+	}
+	
+}
+
